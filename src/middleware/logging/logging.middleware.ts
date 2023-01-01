2e902e11df4b959da74f7065f47f583f7f32fead
Cesar Bacca <bacca2095@gmail.com>
2022-12-31 19:24:58 -0500

    feat: agregado docker compose

--- src/middleware/logging/logging.middleware.ts
+++ src/middleware/logging/logging.middleware.ts
@@ -5,6 +5,7 @@ import { Request, Response } from 'express';
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
   private logger = new Logger();
+
   use(req: Request, res: Response, next: () => void) {
     this.logger.log(
       JSON.stringify({
